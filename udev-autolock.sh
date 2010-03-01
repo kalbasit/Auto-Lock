2436cfe037ee2e50464552712afc73f5174c71bb
Wael Nasreddine <wael.nasreddine@gmail.org>
2010-03-01 22:15:44 +0100

    - Add configuration support. - Indent with tabs not spaces.

--- udev-autolock.sh
+++ udev-autolock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-#   vim:ft=sh:fenc=UTF-8:ts=4:sts=4:sw=4:expandtab:foldmethod=marker:foldlevel=0:
+#   vim:ft=sh:fenc=UTF-8:ts=4:sts=4:sw=4:noexpandtab:foldmethod=marker:foldlevel=0:
 #
 #   Copyright (c) 2007 Wael Nasreddine <wael.nasreddine@gmail.com>
 #
@@ -21,15 +21,23 @@
 #
 
 ####
+# Configurations
+
+if [ -f "/etc/udev-autolock.conf" ]; then
+	source /etc/udev-autolock.conf
+else
+	echo "Configuration file not found. /etc/udev-autolock.conf"
+	exit 1
+fi
+
+#
+####
+
+####
 # Environment
 
-USER="wael"
 ACTION="${1}"
 DEVICE="/dev/${2}"
-OFFSET=131072
-SIZE=5120
-#PID_FILE=/tmp/xlock.pid
-SHA512="db5fddfa816dcf7620c13816337e236042103d58e01ec7bb05c6ae61e0a51eabdf7605f16f6681fe8a988823f2ff7f6361c9bd845dc0d1f87bb098463209b5d9"
 
 #
 ####
@@ -39,31 +47,35 @@ SHA512="db5fddfa816dcf7620c13816337e236042103d58e01ec7bb05c6ae61e0a51eabdf7605f1
 
 function lockDown()
 {
-    # Get the PID of the running lockDown
-    userId="$(id -u ${USER})"
-    PID="$(pgrep -U ${userId} xlock)"
+	for USER in ${USERS[@]}; do
+		# Get the PID of the running lockDown
+		userId="$(id -u ${USER})"
+		PID="$(pgrep -U ${userId} xlock)"
  
-    # Check if a lock is already running
-    if [ -n "${PID}" ]; then
-        echo "Xlock already running with PID: ${PID}"
-        return
-    fi
+		# Check if a lock is already running
+		if [ -n "${PID}" ]; then
+			echo "Xlock already running with PID: ${PID}"
+			return
+		fi
 
-    #if RunningX; then
-        echo "Starting xlock for ${USER}"
-        ( DISPLAY=:0 su -l ${USER} -c '/usr/bin/xlock -mode matrix' & ) &> /dev/null
-    #fi
+		#if RunningX; then
+			echo "Starting xlock for ${USER}"
+			( DISPLAY=:0 su -l ${USER} -c '/usr/bin/xlock -mode matrix' & ) &> /dev/null
+		#fi
+	done
 }
 
 function unlockDown()
 {
-    # Get the PID of the running lockDown
-    userId="$(id -u ${USER})"
-    PID="$(pgrep -U ${userId} xlock)"
+	for USER in ${USERS[@]}; do
+		# Get the PID of the running lockDown
+		userId="$(id -u ${USER})"
+		PID="$(pgrep -U ${userId} xlock)"
 
-    if [ -n "${PID}" ]; then
-        kill -9 "${PID}" > /dev/null 2>&1
-    fi
+		if [ -n "${PID}" ]; then
+			kill -9 "${PID}" > /dev/null 2>&1
+		fi
+	done
 }
 
 #
@@ -75,30 +87,35 @@ function unlockDown()
 
 # Make sure the sd modules
 for i in sdhci sdhci_pci mmc_core mmc_block; do
-    modprobe ${i} > /dev/null 2>&1
+	modprobe ${i} > /dev/null 2>&1
 done
 
 # Start the check
 if [ "${ACTION}" == "remove" ]; then
-    # The device has been removed, LOCKDOWN!
-    lockDown
+	# The device has been removed, LOCKDOWN!
+	echo "The device has been removed, calling lockDown."
+	lockDown
 elif [ "${ACTION}" == "add" ]; then
-    TEMP=`mktemp /tmp/secret.XXXX`
+	echo "The device has been inserted, verifying the data."
 
-    # Read the secret key
-    dd if=${DEVICE} of=${TEMP} bs=1 skip=${OFFSET} count=${SIZE} > /dev/null 2>&1
+	TEMP=`mktemp /tmp/secret.XXXX`
 
-    NEW_SHA=`sha512sum ${TEMP} | awk '{print $1}'`
+	# Read the secret key
+	dd if=${DEVICE} of=${TEMP} bs=1 skip=${OFFSET} count=${SIZE} > /dev/null 2>&1
 
-    shred --remove --zero ${TEMP}
+	NEW_SHA=`sha512sum ${TEMP} | awk '{print $1}'`
 
-    if [ "${SHA512}" != "${NEW_SHA}" ]; then
-        # The device is not a device I'm aware of. LOCKDOWN!
-        lockDown
-    else
-        # The device is the *one* unlock everything
-        unlockDown
-    fi
+	shred --remove --zero ${TEMP}
+
+	if [ "${SHA512}" != "${NEW_SHA}" ]; then
+		# The device is not a device I'm aware of. LOCKDOWN!
+		echo "The data are invalid, calling lockDown."
+		lockDown
+	else
+		# The device is the *one* unlock everything
+		echo "The data are valid, calling unlockDown."
+		unlockDown
+	fi
 fi
 
 #
